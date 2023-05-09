3d04c6a2f15b9f06d90d2f139ed435190a6d1d07
shanny3220 <shanny3220@163.com>
2023-05-09 10:04:10 +0800

    first commit

---
+++ src/main/java/com/hics/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.hics.git;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, May!");
+    }
+}
